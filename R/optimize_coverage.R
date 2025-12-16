f54708def1d8b8028a82272a4461bc0541ada6ee
B-Nilson <nilson.brayden@gmail.com>
2025-12-15 16:43:26 -0800

    use quoted col name instead of .data for unnest

--- R/optimize_coverage.R
+++ R/optimize_coverage.R
@@ -150,7 +150,7 @@ get_covered <- function(install_at, to_cover, cover_distance) {
   to_cover |>
     sf::st_is_within_distance(y = install_at, dist = cover_distance) |>
     tibble::enframe(name = "to_cover_id", value = "matches") |>
-    tidyr::unnest(.data$matches) |>
+    tidyr::unnest("matches") |>
     dplyr::rename(install_at_id = .data$matches)
 }
 
