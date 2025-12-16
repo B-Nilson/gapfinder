375dcbab34eb3e96ef3a57d40b671eacbcf43c90
B-Nilson <nilson@unbc.ca>
2025-12-16 10:49:04 -0800

    fix deprecated tidyselect with .data

--- R/optimize_coverage.R
+++ R/optimize_coverage.R
@@ -151,7 +151,7 @@ get_covered <- function(install_at, to_cover, cover_distance) {
     sf::st_is_within_distance(y = install_at, dist = cover_distance) |>
     tibble::enframe(name = "to_cover_id", value = "matches") |>
     tidyr::unnest("matches") |>
-    dplyr::rename(install_at_id = .data$matches)
+    dplyr::rename(install_at_id = "matches")
 }
 
 add_weight_column <- function(
