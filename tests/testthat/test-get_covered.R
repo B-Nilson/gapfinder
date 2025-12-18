bf55b50c65c5aba3805b9b2c6ad389aac9fabf40
B-Nilson <nilson@unbc.ca>
2025-12-18 09:55:15 -0800

    add basic test

---
+++ tests/testthat/test-get_covered.R
@@ -0,0 +1,13 @@
+test_that("test case works", {
+  test_case <- test_path("fixtures", "test-case.rds") |>
+    readRDS()
+
+  get_covered(
+    install_at = test_case$install_at,
+    to_cover = test_case$to_cover,
+    cover_distance = test_case$cover_distance
+  ) |> 
+    expect_silent() |>
+    print() |> 
+    expect_snapshot()
+})
